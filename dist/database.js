50c60bdc8ca23a2001c9d35a25a19f2ee52c046d
jnieto-ncf <jnieto@negoziacf.com>
2023-02-27 08:43:31 -0500

    clean var

--- dist/database.js
+++ dist/database.js
@@ -5,9 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const config_1 = __importDefault(require("./config/config"));
-const user = 'cafezen_admin';
-const password = 'Daryin231508';
-const bdatos = 'WorkManager';
 //setting options DB
 const DBOption = {
     autoIndex: true,

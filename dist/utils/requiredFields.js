1bfcd017aaba7cb58e8e0ad5535639341e1c826d
jnieto-ncf <jnieto@negoziacf.com>
2023-02-27 08:27:35 -0500

    required fields

--- dist/utils/requiredFields.js
+++ dist/utils/requiredFields.js
@@ -3,13 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validationLogin = exports.validationNewUser = void 0;
 const express_validator_1 = require("express-validator");
 exports.validationNewUser = [
-    (0, express_validator_1.check)('email', 'El correo electrónico es obligatorio').notEmpty(),
-    (0, express_validator_1.check)('email', 'El correo electrónico no es válido').isEmail(),
-    (0, express_validator_1.check)('password', 'La contraseña es obligatoria').notEmpty(),
-    (0, express_validator_1.check)('name', 'El nombre es obligatorio').notEmpty(),
-    (0, express_validator_1.check)('lastname', 'El apellido es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('email', 'El correo electrónico (email) es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('email', 'El correo electrónico (email) no es válido').isEmail(),
+    (0, express_validator_1.check)('password', 'La contraseña (password) es obligatoria').notEmpty(),
+    (0, express_validator_1.check)('name', 'El nombre (name) es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('lastname', 'El apellido (lastname) es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('phone', 'El teléfono (phone) es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('country', 'El pais (country) es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('geographicZone', 'El departamento (geographicZone) es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('city', 'La ciudad (city) es obligatoria').notEmpty(),
+    (0, express_validator_1.check)('address', 'La dirección (address) es obligatoria').notEmpty(),
+    (0, express_validator_1.check)('company', 'La empresa (company) es obligatoria').notEmpty(),
 ];
 exports.validationLogin = [
-    (0, express_validator_1.check)('email', 'El correo electrónico es obligatorio').notEmpty(),
-    (0, express_validator_1.check)('password', 'La contraseña es obligatoria').notEmpty(),
+    (0, express_validator_1.check)('email', 'El correo electrónico (email) es obligatorio').notEmpty(),
+    (0, express_validator_1.check)('email', 'El correo electrónico (email) no es válido').isEmail(),
+    (0, express_validator_1.check)('password', 'La contraseña (password) es obligatoria').notEmpty(),
 ];
--- src/utils/requiredFields.ts
+++ src/utils/requiredFields.ts
@@ -1,14 +1,21 @@
 import { check } from "express-validator";
 
 export const validationNewUser = [
-    check('email', 'El correo electrónico es obligatorio').notEmpty(),
-    check('email', 'El correo electrónico no es válido').isEmail(),
-    check('password', 'La contraseña es obligatoria').notEmpty(),
-    check('name', 'El nombre es obligatorio').notEmpty(),
-    check('lastname', 'El apellido es obligatorio').notEmpty(),
+    check('email', 'El correo electrónico (email) es obligatorio').notEmpty(),
+    check('email', 'El correo electrónico (email) no es válido').isEmail(),
+    check('password', 'La contraseña (password) es obligatoria').notEmpty(),
+    check('name', 'El nombre (name) es obligatorio').notEmpty(),
+    check('lastname', 'El apellido (lastname) es obligatorio').notEmpty(),
+    check('phone', 'El teléfono (phone) es obligatorio').notEmpty(),
+    check('country', 'El pais (country) es obligatorio').notEmpty(),
+    check('geographicZone', 'El departamento (geographicZone) es obligatorio').notEmpty(),
+    check('city', 'La ciudad (city) es obligatoria').notEmpty(),
+    check('address', 'La dirección (address) es obligatoria').notEmpty(),
+    check('company', 'La empresa (company) es obligatoria').notEmpty(),
 ];
 
 export const validationLogin = [
-    check('email', 'El correo electrónico es obligatorio').notEmpty(),
-    check('password', 'La contraseña es obligatoria').notEmpty(),
+    check('email', 'El correo electrónico (email) es obligatorio').notEmpty(),
+    check('email', 'El correo electrónico (email) no es válido').isEmail(),
+    check('password', 'La contraseña (password) es obligatoria').notEmpty(),
 ];
